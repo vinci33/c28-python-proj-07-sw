ba16d554f139285f6de348bf23e6588c48f2cdcb
KDL <kennethdouglasslai@gmail.com>
2023-11-09 07:57:19 +0800

    update index.js index.html app.py

--- python/app.py
+++ python/app.py
@@ -5,7 +5,6 @@ from routes import routes
 from dotenv import load_dotenv
 import os
 
-# Load the environment variables
 load_dotenv()
 key = os.getenv('KEY')
 print(key)
@@ -13,18 +12,15 @@ print(key)
 
 app = Sanic("BAD_project_team7_2023_11_05")
 
-# Set up the OpenAI API key
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/speech-to-text', methods=['POST'])
 async def speech_to_text(request):
-    # Get the audio data from the request body
     audio_data = request.json.get('audio_data')
 
-    # Use the OpenAI API to convert the audio data to text
     response = openai.SpeechToText.create(audio_data=audio_data)
 
-    # Return the response from the OpenAI API
     return json({'response': response.text})
 
 app.blueprint(routes)
