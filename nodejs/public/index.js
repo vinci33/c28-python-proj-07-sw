0dcd0bd87bb2ae8ebe7da69553bdda8c4dda5258
KDL <kennethdouglasslai@gmail.com>
2023-11-12 07:26:51 +0800

    Refactor chatbot functionality and add WebSocket server.

--- nodejs/public/index.js
+++ nodejs/public/index.js
@@ -1,49 +1,228 @@
+const socket = new WebSocket('ws://localhost:8000/ws');
+console.log("socket:", socket);
+
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+let userMessage = null; // Variable to store user's message
+
+const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
+
+
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">person</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+const generateResponse = (chatElement) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = chatElement.querySelector("p");
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: userMessage }],
+        })
+    }
+    // Send POST request to API, get response and set the reponse as paragraph text
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content.trim();
+    }).catch(() => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if (!userMessage) return;
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        // Display "Thinking..." message while waiting for the response
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
+
+// async function startRecording() {
+//     try {
+//         let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+//         const mediaRecorder = new MediaRecorder(stream);
+//         let chunks = [];
+
+//         mediaRecorder.ondataavailable = function (event) {
+//             chunks.push(event.data);
+//         };
+
+//         mediaRecorder.onstop = async function () {
+//             let blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+//             chunks = [];
+
+//             let arrayBuffer = await blob.arrayBuffer();
+//             let audio_data = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+
+//             try {
+//                 // Send the audio data to the Python backend
+//                 let response = await fetch('http://localhost:8000/speech-to-text', {
+//                     method: 'POST',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                     },
+//                     body: JSON.stringify({ audio_data: audio_data }),
+//                 });
+
+//                 let data = await response.json();
+//                 console.log(data);
+//             } catch (error) {
+//                 console.error('Error:', error);
+//             }
+//         };
+
+//         let button = document.querySelector('#start');
+
+//         button.onmousedown = button.ontouchstart = function () {
+//             mediaRecorder.start();
+//         };
+
+//         button.onmouseup = button.ontouchend = function () {
+//             mediaRecorder.stop();
+//         };
+//     } catch (err) {
+//         console.log('getUserMedia error: ', err);
+//     }
+// }
+
+
+
+const recordButton = document.querySelector("#recordButton");
+const recordButtonChatbox = document.querySelector("#recordButtonChatbox");
+recordButton.addEventListener("touchstart", startRecording);
+recordButton.addEventListener("touchend", stopRecording);
+recordButton.addEventListener("mousedown", startRecording);
+recordButton.addEventListener("mouseup", stopRecording);
+let mediaRecorder;
+let audioChunks = [];
+let isRecording = false;
+
+
 async function startRecording() {
-    try {
-        let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-        const mediaRecorder = new MediaRecorder(stream);
-        let chunks = [];
-
-        mediaRecorder.ondataavailable = function (event) {
-            chunks.push(event.data);
-        };
-
-        mediaRecorder.onstop = async function () {
-            let blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
-            chunks = [];
-
-            let arrayBuffer = await blob.arrayBuffer();
-            let audio_data = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
-
-            try {
-                // Send the audio data to the Python backend
-                let response = await fetch('http://localhost:8000/speech-to-text', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ audio_data: audio_data }),
-                });
-
-                let data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-
-        let button = document.querySelector('#start');
-
-        button.onmousedown = button.ontouchstart = function () {
+    if (!isRecording) {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start();
-        };
-
-        button.onmouseup = button.ontouchend = function () {
-            mediaRecorder.stop();
-        };
-    } catch (err) {
-        console.log('getUserMedia error: ', err);
+            isRecording = true;
+            recordButton.classList.add("active");
+            audioChunks = [];
+            mediaRecorder.addEventListener("dataavailable", e => {
+                audioChunks.push(e.data);
+            });
+        } catch (err) {
+            console.log("Start recording fail:", err);
+        }
     }
 }
 
-startRecording();
+function stopRecording() {
+    if (mediaRecorder && isRecording) {
+        mediaRecorder.stop();
+        isRecording = false;
+        recordButton.classList.remove("active");
+        mediaRecorder.addEventListener("stop", async () => {
+            const audioBlob = new Blob(audioChunks);
+            try {
+                await sendDataToServer(audioBlob);
+            } catch (err) {
+                console.log("Stop recode fail:", err);
+            }
+        });
+    }
+}
+
+async function sendDataToServer(blob) {
+    try {
+        const formData = new FormData();
+        formData.append("audio", blob);
+        const response = await fetch("/upload-audio", {
+            method: "POST",
+            body: formData
+        });
+        const data = await response.text();
+        document.getElementById("message").innerText = "接收到的訊息: " + data;
+        return data;
+    } catch (err) {
+        console.log("Data to server failed :", err);
+        throw err;
+    }
+}
+
+// startRecording();
+
+
+
+
+socket.onopen = function (event) {
+    socket.send(JSON.stringify({ id: 1, type: 'text', data: 'Hello, server!' }));
+};
+
+socket.onerror = function (event) {
+    console.error("WebSocket 錯誤:", event);
+};
+
+function sendTextMessage(text, source) {
+    socket.send(JSON.stringify({ source: source, type: 'text', data: text }));
+}
+// sendTextMessage('Hello, server from .send !', 'A');
+
+function sendTextMessageA(text) {
+    socket.send(JSON.stringify({ source: 'A', type: 'text', data: text }));
+}
+sendTextMessageA('Hello, server from .sendA !');
+
+socket.onmessage = function (event) {
+    const message = JSON.parse(event.data);
+    if (message.type === 'textFromServer') {
+        console.log('收到文字訊息: ' + message.data);
+    }
+};
+
+async function sendVoiceBlob(blob) {
+    const arrayBuffer = await blob.arrayBuffer();
+    socket.send(arrayBuffer);
+}
--- python/app.py
+++ python/app.py
@@ -1,5 +1,10 @@
 import openai
-from sanic import Sanic
+import whisper
+import json
+import asyncio
+import websockets
+
+from sanic import Sanic 
 from sanic_ext import Extend
 from sanic.response import json
 from routes import routes
@@ -7,17 +12,82 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-key = os.getenv('KEY')
-print(key)
+openai.api_base = "https://api.gpt.tecky.ai/v1"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+print(openai.api_key)
 
-
-app = Sanic("BAD_project_team7_2023_11_05")
+# app = Sanic("BAD_project_team7_2023_11_05")
 
 app = Sanic(__name__)
 app.config.CORS_ORIGINS = "http://localhost:8080"
 Extend(app)
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+async def websocket_handler(websocket, path):
+    print("WebSocket handler started")
+    while True:
+       
+
+        try:
+            message = await websocket.recv()
+            data = json.loads(message) 
+            print(message ,data)
+            if data['type'] == 'whisper_request':
+            # Process Whisper
+                whisper_response = process_with_whisper(data['data'])
+                await websocket.send(json.dumps({'type': 'whisper_response', 'data': whisper_response}))
+        
+            elif data['type'] == 'messageTypeA':
+            # Process ChatGPT request
+                chatgpt_response = process_with_chatgpt(data['data'])
+                await websocket.send(json.dumps({'type': 'chatgpt_response', 'data': chatgpt_response}))
+
+            elif data['source'] == 'A':
+            # Process other types of messages
+            # Replace with actual processing logic
+                message_from_A = data['data']
+                # response = process_messageTypeB(data['data'])
+                await websocket.send(json.dumps({'type': 'messageTypeB_response', 'data': message_from_A}))
+
+        except json.JSONDecodeError:
+            # Handle JSON decode error
+            print("Received non-JSON message")
+        except Exception as e:
+            # Handle other errors
+            print(f"An error occurred: {e}")
+
+def process_with_whisper(audio_data):
+    # Implement Whisper processing
+    audio_file = open("received_audio.wav", "wb")
+    audio_file.write(audio_data)
+    audio_file.close()
+    model = whisper.load_model("base")
+    result = model.transcribe("received_audio.wav")
+    print(result["text"])
+    # Return processed text
+    return result["text"]
+
+def process_with_chatgpt(text):
+    # Implement ChatGPT processing
+    # Return ChatGPT response
+    return "Response from ChatGPT"
+
+def process_messageTypeB(data):
+    # Implement your processing for messageTypeB
+    return "Processed data for messageTypeB"
+
+
+        # audio_data = await websocket.recv()
+        
+        # # 處理接收到的音頻數據
+        # audio_file = open("received_audio.wav", "wb")
+        # audio_file.write(audio_data)
+        # audio_file.close()
+
+        # # 使用 Whisper 轉換語音為文字
+        # model = whisper.load_model("base")
+        # result = model.transcribe("received_audio.wav")
+        # await websocket.send(result["text"])
+
 
 @app.route('/speech-to-text', methods=['POST'])
 async def speech_to_text(request):
@@ -30,4 +100,6 @@ async def speech_to_text(request):
 app.blueprint(routes)
 
 if __name__ == "__main__":
+    print("Starting server") 
+    app.add_websocket_route(websocket_handler, '/ws')
     app.run(host="0.0.0.0", port=8000, workers=1)
