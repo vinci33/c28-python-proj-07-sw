0afa5cb96c96735f8f2c0a0baeec00b5f33c3612
Jason Yim <jason20210106@gmail.com>
2023-11-17 10:45:41 +0800

    deploy

--- nodejs/main.ts
+++ nodejs/main.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-// import expressSession from 'express-session';
+import expressSession from 'express-session';
 import dotenv from 'dotenv';
 import path from 'path';
 import { routes } from './routes';
@@ -14,26 +14,26 @@ export const knex = require('knex')(config[process.env.NODE_ENV || 'development'
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(
-//     expressSession({
-//         secret: "kennethAndJasonWithJames",
-//         resave: true,
-//         saveUninitialized: true,
-//     })
-// );
+app.use(
+    expressSession({
+        secret: "kennethAndJasonWithJames",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
 
-// declare module 'express-session' {
-//     export interface SessionData {
-//         userId?: number;
-//         customer_id?: number;
-//     }
-// }
+declare module 'express-session' {
+    export interface SessionData {
+        userId?: number;
+        customer_id?: number;
+    }
+}
 
 
 
 app.use('/', routes);
-app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(express.static(path.join(__dirname, 'public', "html")))
 
 app.use
